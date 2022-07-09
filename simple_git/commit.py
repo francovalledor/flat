fa55f6fde18b2062c25e20a1a72de6d3e1886896
Franco Valledor <francovalledor@gmail.com>
2022-07-09 17:41:59 -0300

    SimpleGit: Create commit class

---
+++ simple_git/commit.py
@@ -0,0 +1,35 @@
+from git import Commit as RepoCommit
+
+class Commit:
+    def __init__(self, repo_commit: RepoCommit) -> None:
+        self.__commit = repo_commit
+
+    @property
+    def author(self):
+        return self.__commit.author
+
+    @property
+    def hash(self):
+        return self.__commit.hexsha
+
+    @property
+    def message(self):
+        return self.__commit.message.strip()
+
+    @property
+    def datetime(self):
+        return self.__commit.committed_datetime
+
+    @property
+    def affected_files(self):
+        return self.__commit.stats.files
+    
+    @property
+    def total_changes(self):
+        return self.__commit.stats.total
+
+    def __str__(self) -> str:
+        return f"\nMessage: {self.message}\nAuthor: {self.author}\nDate: {self.datetime}\nHash: {self.hash}\n"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
