78edd1b098901b550d033fda4c888a59cdb9b212
Franco Valledor <francovalledor@gmail.com>
2022-07-10 18:51:17 -0300

    add branch_name validation to PR serializer

--- backend/apiv1/serializers/pull_requests.py
+++ backend/apiv1/serializers/pull_requests.py
@@ -1,13 +1,19 @@
 from rest_framework.serializers import ModelSerializer, ValidationError
 from apiv1.models import PullRequest
+from apiv1.utils import get_branches_names
 
+
+def validate_branch_name(branch_name):
+    branches = get_branches_names()
+    if branch_name not in branches:
+        raise ValidationError(f"Branch does not exist: '{branch_name}'")
 
 class PullRequestSerializer(ModelSerializer):
     def validate_source(self, source):
-        raise ValidationError("Branch name does not exist")
-    
-    def validate_destination(self, source):
-        raise ValidationError("Branch name does not exist")
+        validate_branch_name(source)
+        
+    def validate_destination(self, destination):
+        validate_branch_name(destination)
     
     class Meta:
         model = PullRequest
