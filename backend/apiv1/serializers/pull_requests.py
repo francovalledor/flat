e07c3550442180ec13b8bcbced83eac662ee6dfe
Franco Valledor <francovalledor@gmail.com>
2022-07-10 20:01:11 -0300

    Add validations to PullRequestSerializer

--- backend/apiv1/serializers/pull_requests.py
+++ backend/apiv1/serializers/pull_requests.py
@@ -1,5 +1,6 @@
+import re
 from rest_framework.serializers import ModelSerializer, ValidationError
-from apiv1.models import PullRequest
+from apiv1.models import PullRequest, PR_statuses
 from apiv1.utils import get_branches_names
 
 
@@ -8,6 +9,10 @@ def validate_branch_name(branch_name):
     if branch_name not in branches:
         raise ValidationError(f"Branch does not exist: '{branch_name}'")
 
+def validate_branches_are_different(source, destination):
+    if source == destination:
+        raise ValidationError(f"Source ({source}) and destination ({destination}) should be different")
+
 class PullRequestSerializer(ModelSerializer):
     def validate_source(self, source):
         validate_branch_name(source)
@@ -15,6 +20,12 @@ class PullRequestSerializer(ModelSerializer):
     def validate_destination(self, destination):
         validate_branch_name(destination)
     
+    def validate_author_email(self, email):
+        regex = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
+        
+        if not re.search(regex, email):
+            raise ValidationError(f"Invalid email: '{email}'")
+
     class Meta:
         model = PullRequest
         fields = [
