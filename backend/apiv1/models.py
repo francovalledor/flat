9cc5fcd798da47d7129f358f5ebc3dfb0a72ee89
Franco Valledor <francovalledor@gmail.com>
2022-07-10 02:57:58 -0300

    Create PullRequest model

--- backend/apiv1/models.py
+++ backend/apiv1/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class PR_statuses(models.TextChoices):
+    OPEN = 'OPEN'
+    CLOSED = 'CLOSED'
+    MERGED = 'MERGED'
+
+class PullRequestsModels(models.Model):
+
+    source = models.CharField(max_length=255)
+    destination = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    message = models.TextField(default="")
+
+    status = models.CharField(
+            choices=PR_statuses.choices,
+            default=PR_statuses.OPEN,
+            max_length=6
+        )
+
+    author_name = models.CharField(max_length=255)
+    author_email = models.CharField(max_length=255)
+    
