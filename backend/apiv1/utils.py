126869b7c9636f3840726ce9dd2de117b80ac3ad
Franco Valledor <francovalledor@gmail.com>
2022-07-10 14:51:06 -0300

    Add NotFound json response

--- backend/apiv1/utils.py
+++ backend/apiv1/utils.py
@@ -1,5 +1,8 @@
 from apiv1.libs.simple_git import SimpleGit, Commit, Branch
 from apiv1.serializers import serialize_branch_simple, serialize_branch_details, serialize_commit
+from apiv1.libs.simple_git.exceptions import InvalidBranchNameException, InvalidCommitHashException
+from rest_framework.exceptions import NotFound
+
 
 git = SimpleGit()
 
@@ -8,8 +11,11 @@ def get_all_branches():
 
 
 def get_branch_with_commits(branch_name):
-    commits = git.list_commits(branch_name)
-    branch = git.get_branch_by_name(branch_name)
+    try:
+        branch = git.get_branch_by_name(branch_name)
+        commits = git.list_commits(branch_name)
+    except InvalidBranchNameException as error:
+        raise NotFound(f"branch: '{branch_name}'")
 
     return serialize_branch_details(branch, commits)
 
@@ -22,6 +28,9 @@ def get_all_commits():
 
 
 def get_commit(commit_hash):
-    commit = git.get_commit(commit_hash)
+    try:
+        commit = git.get_commit(commit_hash)
+    except InvalidCommitHashException as error:
+        raise NotFound(f"Commit hash: '{commit_hash}'")
     
     return serialize_commit(commit)
