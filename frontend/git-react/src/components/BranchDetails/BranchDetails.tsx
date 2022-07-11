70df1a9589e4437ea0016ab9d329f9df8847be34
Franco Valledor <francovalledor@gmail.com>
2022-07-11 09:58:19 -0300

    clean BranchDetails

--- frontend/git-react/src/components/BranchDetails/BranchDetails.tsx
+++ frontend/git-react/src/components/BranchDetails/BranchDetails.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getBranch, getCommit } from "../../api/api";
-import { BranchDetails as BranchDetailsType, Commit } from "../../types/types";
-import { formatDate } from "../../utils/dates";
+import { getBranch } from "../../api/api";
+import { BranchDetails as BranchDetailsType } from "../../types/types";
 import CommitList from "../CommitList/CommitList";
 
 const BranchDetails: React.FC = () => {
