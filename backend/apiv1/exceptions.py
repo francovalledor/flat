141f3af74efb7f837d95b70fc8e98b268bac12d5
Franco Valledor <francovalledor@gmail.com>
2022-07-10 21:05:42 -0300

    create UncommittedChangesAPIException

--- backend/apiv1/exceptions.py
+++ backend/apiv1/exceptions.py
@@ -1,7 +1,14 @@
 from rest_framework.exceptions import APIException
 from rest_framework import status
 
+
 class MergeConflictsException(APIException):
     status_code = status.HTTP_409_CONFLICT
     default_detail = 'Merge conflicts'
     default_code = 'merge_conflicts'
+
+
+class UncommittedChangesAPIException(APIException):
+    status_code = status.HTTP_412_PRECONDITION_FAILED
+    default_detail = 'You have uncommitted changes, please clean your workspace and try again'
+    default_code = 'uncommitted_changes'
