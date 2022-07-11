1a93713513fcf6bd5085e707a8583e747a0bb479
Franco Valledor <francovalledor@gmail.com>
2022-07-10 21:11:04 -0300

    choose the serializer according to the http method

--- backend/apiv1/views/pull_requests.py
+++ backend/apiv1/views/pull_requests.py
@@ -1,13 +1,21 @@
 from rest_framework.viewsets import mixins, GenericViewSet
 from apiv1.models import PullRequest
-from apiv1.serializers.pull_requests import PullRequestBaseSerializer
+from apiv1.serializers.pull_requests import PullRequestBaseSerializer, PullRequestCreateSerializer
 
 class PullRequestViewSet(
-    mixins.CreateModelMixin,
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.ListModelMixin,
-    GenericViewSet,
+        mixins.CreateModelMixin,
+        mixins.RetrieveModelMixin,
+        mixins.UpdateModelMixin,
+        mixins.ListModelMixin,
+        GenericViewSet,
     ):
     queryset = PullRequest.objects.all()
-    serializer_class = PullRequestBaseSerializer
+    
+    def get_serializer_class(self):
+        DEFAULT_SERIALIZER = PullRequestBaseSerializer
+
+        SERIALIZERS_MAP = { 'POST': PullRequestCreateSerializer }
+
+        request_method = self.request.method
+        serializer = SERIALIZERS_MAP.get(request_method, DEFAULT_SERIALIZER)
+        return serializer
