9320774d605a22282be92852abdd00b983bec20a
Franco Valledor <francovalledor@gmail.com>
2022-07-11 09:24:41 -0300

    clean App.tsx

--- frontend/git-react/src/App.tsx
+++ frontend/git-react/src/App.tsx
@@ -1,12 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { get } from "./utils/fetch";
 import './App.css';
-import { closePullRequest, createPullRequest, getBranch, getBranches, getCommit, getCommits, getPullRequest, getPullRequests, mergePullRequest, updatePullRequest } from './api/api';
-import { Branch, Commit, PullRequest } from './types/types';
 import BranchList from './components/BranchList/BranchList';
-import CommitList from './components/CommitList/CommitList';
 import PRList from './components/PRList/PRList';
 import {
   BrowserRouter as Router,
@@ -22,28 +17,6 @@ import CommitListContainer from './components/CommitList/CommitListContainer';
 
 
 function App() {
-  const [branches, setBranches] = useState<Branch[]>([]);
-  const [PRs, setPRs] = useState<PullRequest[]>([]);
-
-  const fetch_commits = async () => {
-    const pull_request = {
-      author_email: 'francovalledor@gmail.com',
-      author_name: 'Franco Valledor',
-      destination: 'frontend',
-      source: 'master',
-      title: 'Hola mundo'
-    }
-    const response = await getBranches();
-    setBranches(response);
-
-    const _prs = await getPullRequests();
-    setPRs(_prs);
-  }
-
-  useEffect(() => {
-    fetch_commits();
-  }, []);
-
   return (
     <div className="App">
       <div className="container p-4 mt-4 shadow">
